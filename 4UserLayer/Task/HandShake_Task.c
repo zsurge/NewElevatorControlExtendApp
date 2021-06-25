2fb11f0bc75f611b7502a54fe6424eb1878cbf9b
zhangduo <zdsurge@163.com>
2021-06-25 17:55:58 +0800

    V1.0.2 使用在山语清辉项目 1.修正未联网一直重启的BUG“

--- 4UserLayer/Task/HandShake_Task.c
+++ 4UserLayer/Task/HandShake_Task.c
@@ -103,7 +103,7 @@ static void vTaskHandShake(void *pvParameters)
 
     while(1)
     {  
-        bsp_HC595Show('E',0,1);
+        bsp_HC595Show('E',0,2);
         vTaskDelay(800);
         bsp_HC595Show(0,0,1);
         vTaskDelay(800);
